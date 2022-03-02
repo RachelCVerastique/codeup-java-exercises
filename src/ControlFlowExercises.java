52b2e1b6776f8b965ef94bf1e727768cd0c00019
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-03-01 22:22:06 -0600

    adds fizzbuzz java

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -43,5 +43,21 @@ public class ControlFlowExercises {
 //
 //            System.out.println("Your number is  " + i);
 //        }
+
+//        //Fizz Buzz
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if(i % 5 == 0){
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0 ){
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+
+
     }
 }
