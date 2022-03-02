717a3a5af850d26ff3a3ef1a2b3d4de7e6e93000
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-03-01 18:58:37 -0600

    adds java while loop
    git status

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,28 @@
+
+
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %,.2f", pi);
+
+
+        Scanner input = new Scanner(System.in);
+
+//        int myNum = input.nextInt();
+//        System.out.println("integer");
+
+//        String str = input.nextLine();
+//        String str2 = input.nextLine();
+//        String str3 = input.nextLine();
+//        System.out.print("There you are Miss Tuffsy ");
+
+
+
+
+
+
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,44 @@
 public class HelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello, World!");
-        System.out.print("Hello");
-        System.out.print(", World!");
-        System.out.print("\n");
+//        System.out.print("Hello");
+//        System.out.print(", World!");
+//        System.out.print("\n");
+
+//        int myFavoriteNumber = 2;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "3.14159";
+//        System.out.println(myString);
+//
+//          long myNumber;
+//        System.out.println(myNumber);
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+//        int class =
+
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        System.out.println(three);
+
+//        int three = (int) "three";
+//
+//        System.out.println(three);
+//
+
+
     }
 }
