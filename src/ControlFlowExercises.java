af9d09e5154c49591df21cd1ad92465af2109e66
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-03-01 22:07:46 -0600

    adds java loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,47 @@
+import java.util.function.ToDoubleBiFunction;
+
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        int i = 5;
-            while(i <= 15) {
-                System.out.println(i++);
-        }
+//        int i = 5;
+//            while(i <= 15) {
+//                System.out.println(i++);
+//        }
+//        int i = 0;
+//        do {
+//            System.out.println("Your number is : " + i);
+//            i+=2;
+//        } while( i <= 100 );
+//        int i = 100;
+//        do {
+//            System.out.println("Your number is : " + i);
+//            i-=5;
+//        } while( i >= -10 );
+//
+//        HALP***********************************************************
+//        int i = 2;
+//        do {
+//            System.out.println("Your number is : " + i);
+//            i+= Math.pow(i, 2);
+//
+//        } while( i <= 1000000 );
+
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.println("Your number is  " + i);
+//        }
+
+//        for (int i = 0; i <= 100; i+=2) {
+//            System.out.println("Your number is  " + i);
+//        }
+
+//        for (int i = 100; i >= -10; i-=5) {
+//            System.out.println("Your number is  " + i);
+//        }
+
+//      HALP***************************************************
+//        for (int i = 2; i <= 1000000; i+=2) {
+//
+//            System.out.println("Your number is  " + i);
+//        }
     }
 }
